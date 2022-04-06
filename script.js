625bf7eba2608c703a5943bb06faea96976b0c8e
JnBanks <joku.banks@gmail.com>
2022-04-05 22:11:08 -0700

    discovered that questions in order is just as difficult, reverting to random question function

--- script.js
+++ script.js
@@ -27,8 +27,113 @@ function setTime() {
     }
   }, 1000);
 }
+function genNumAndAdd (questionsAsked) {
+  var rando = Math.floor(Math.random() * 10);
+  questionsAsked.push(rando);
+}
+  function checkRedundancy (value, questionsAsked) {
+    
+      for (var i = 0; i < questionsAsked.length; i++) {
+          var question = questionsAsked[i];
+          if (question = value) {
+              
+            } else if (i = questionsAsked.length) {
+                //show the final score!
+                console.log('No more questions!')
+              }
+            }
+            return;
+          }
 
-// function firstQuestion () {
+
+//ask question (no more messing with the random question business)
+function askQuestion () {
+  
+  for (var i = 0; i < questionBank.length; i++) {
+    var currentQuestion = document.getElementById(i);
+    currentQuestion.setAttribute("style", "display: block", "margin: auto");
+  }
+}
+
+
+//eventlistener for clicking a list item/answer and then determining what to do
+choice.addEventListener("click", function() {
+          //check to see if correct or incorrect
+          var event = event.target;
+          if (event.target = correctChoice) {
+            //  if correct, add to score
+            score++;
+            //hide the question and answers
+            document.ul.setAttribute("style", "display: none");
+            //find a new question
+            askQuestion();
+          } else {
+            secondsLeft - 3;
+            askQuestion();
+          }
+          
+        })
+
+        //function that shows final score
+        function finalScore (event) {
+          event.preventDefault();
+  //stop the timer and store number of seconds remaining
+  //get the number from score array
+  //add the seconds remaining to the score value
+  if (secondsLeft > 0) {
+    score + secondsLeft;
+    secondsLeft = 0;
+    getStats();
+  } else if (seconds = 0) {
+    getStats();
+  }
+  //they also have the option to play again
+}
+
+
+function getStats (stats) {
+  //display the score and an input field for their initials
+  var stats = prompt("Your score is " + score + ". Please enter your initials.")
+  //if they don't input anything, alert them saying they have to input something
+  if (stats = "") {
+    alert("Please enter your initials")
+  } else {
+    localStorage.setItem(stats, score)
+    console.log(stats, score);
+  }
+}
+
+//Function that shows the scoreboard history
+
+
+//Start button event----------
+startBtn.addEventListener("click", function() {
+  //we want to display the first question
+  
+  //  remove the start button
+  startBtn.setAttribute("style", "display: none");
+  //  remove the header
+  openingH1.setAttribute("style", "display: none")
+  //  start the timer
+  setTime();
+  // console.log(questionBank);
+  // console.log(questionBank.length);
+  askQuestion();
+  console.log("yeah");
+})
+
+
+//show final score if user answers all ten questions
+
+
+//\\\\\\\\\\\\\\\\\\\\\\\\
+//create a function for hiding elements??????????
+// function hideElement () {
+  
+  // }
+  
+  
+  // function firstQuestion () {
 //   //produce a random number between 0 and 9, call it variable rando
 //   //var rando = Math.floor(Math.random() * 10);
 //   //questionsAsked = rando;
@@ -59,118 +164,32 @@ function setTime() {
 
 //}
 
-//============================================================
-//ask question (no more messing with the random question business)
-function askQuestion () {
-
-  for (var i = 0; i < questionBank.length; i++) {
-    var currentQuestion = document.getElementById(i);
-    currentQuestion.setAttribute("style", "display: block", "margin: auto");
-  }
-}
 
 //what about a function that produces a random number and ensures it is not a number that has been previously rendered?
 // function firstNumber (questionsAsked) {
-//   var rando = Math.floor(Math.random() * 10);
+  //   var rando = Math.floor(Math.random() * 10);
 //   questionsAsked.push(rando);
 // }
-  
+
 
 
 
 //Do not repeat a question
 // function checkRedundancy (value, questionsAsked) {
-//   var status = 'Not Asked';
-
-//   for (var i = 0; i < questionsAsked.length; i++) {
-//     var question = questionsAsked[i];
-//     if (question = value) {
-//       status = 'Asked';
-//       findQuestion;
-//     } else if (i = questionsAsked.length) {
-//       //show the final score!
-//       console.log('No more questions!')
-//     }
-//   }
-//   return status;
-// }
-
-//check the value of the choice, correct or incorrect
-//function checkAnswer ()
-
-//eventlistener for clicking a list item/answer and then determining what to do
-choice.addEventListener("click", function() {
-//check to see if correct or incorrect
-var event = event.target;
-if (event.target = correctChoice) {
-  //  if correct, add to score
-  score++;
-  //hide the question and answers
-  document.ul.setAttribute("style", "display: none");
-  //find a new question
-  askQuestion();
-} else {
-  secondsLeft - 3;
-  askQuestion();
-}
-
-})
-
-//function that shows final score
-function finalScore (event) {
-  event.preventDefault();
-//stop the timer and store number of seconds remaining
-//get the number from score array
-//add the seconds remaining to the score value
-if (secondsLeft > 0) {
-  score + secondsLeft;
-  secondsLeft = 0;
-  getStats();
-} else if (seconds = 0) {
-  getStats();
-}
-//they also have the option to play again
-}
-
-
-function getStats (stats) {
-  //display the score and an input field for their initials
-  var stats = prompt("Your score is " + score + ". Please enter your initials.")
-  //if they don't input anything, alert them saying they have to input something
-  if (stats = "") {
-    alert("Please enter your initials")
-  } else {
-    localStorage.setItem(stats, score)
-    console.log(stats, score);
-  }
-}
-
-//Function that shows the scoreboard history
-
-
-//Start button event----------
-startBtn.addEventListener("click", function() {
-  //we want to display the first question
+  //   var status = 'Not Asked';
   
-//  remove the start button
-startBtn.setAttribute("style", "display: none");
-//  remove the header
-openingH1.setAttribute("style", "display: none")
-//  start the timer
-setTime();
-// console.log(questionBank);
-// console.log(questionBank.length);
-askQuestion();
-console.log("yeah");
-})
-
-
-//show final score if user answers all ten questions
-
-
-//\\\\\\\\\\\\\\\\\\\\\\\\
-//create a function for hiding elements??????????
-// function hideElement () {
-
-// }
-
+  //   for (var i = 0; i < questionsAsked.length; i++) {
+    //     var question = questionsAsked[i];
+    //     if (question = value) {
+      //       status = 'Asked';
+      //       findQuestion;
+      //     } else if (i = questionsAsked.length) {
+        //       //show the final score!
+        //       console.log('No more questions!')
+        //     }
+        //   }
+        //   return status;
+        // }
+        
+        //check the value of the choice, correct or incorrect
+        //function checkAnswer ()
